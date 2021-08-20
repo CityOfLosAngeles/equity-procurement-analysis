f222a923c84d5b5ad25c1cb77ca2cc1f140a780b
Irene <irenetang14@gmail.com>
2021-08-20 11:06:01 -0700

    save to gsheet

--- src/temporal.py
+++ src/temporal.py
@@ -1,10 +1,10 @@
 import pandas as pd
+import save_files
 
 # Read in generated data from previous notebook
 all_data = pd.read_csv("../data/all_data.csv")
 
-
-def get_percents(timeframes: list, timeframe_column: str = "bid_due_year", to_gsheet: bool = False):
+def get_percents(timeframes: list, timeframe_column: str = "bid_due_year"):
     """
     Returns the % of contracts and % of dollars awarded to DBE, MBE, WBEs
     """
@@ -45,27 +45,26 @@ def get_percents(timeframes: list, timeframe_column: str = "bid_due_year", to_gs
             value.append(percent)
         df[df_dollars_columns[i]] = value
 
-    if(to_gsheet):
-        pass
-
     return df
 
 
 def main():
     # Generate google sheet for yearly analysis
     years = sorted(all_data.bid_due_year.unique())
-    percents_by_year = get_percents(years, "bid_due_year", to_gsheet=True)
+    percents_by_year = get_percents(years, "bid_due_year")
     print(percents_by_year)
 
-    # TODO: write to google sheet
+    # write to google sheet
+    save_files.save_to_gsheet(percents_by_year, "Procurement Data", 3)
 
 
     # Generate google sheet for monthly analysis
     months = sorted(all_data.bid_due_year_month.unique())
-    percents_by_month = get_percents(months, "bid_due_year_month", to_gsheet=True)
+    percents_by_month = get_percents(months, "bid_due_year_month")
     print(percents_by_month)
 
-    # TODO: write to google sheet
+    # write to google sheet
+    save_files.save_to_gsheet(percents_by_month, "Procurement Data", 4)
 
 
 if __name__ == "__main__":
